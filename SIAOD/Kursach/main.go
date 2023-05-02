c3d11efd56fd2c849d3edc9dfb3d57a01c90b328
ghostik-gh <feodor200@mail.ru>
2023-05-02 20:50:21 +0300

    HEad Table

--- SIAOD/Kursach/main.go
+++ SIAOD/Kursach/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,11 +17,12 @@ type Order struct {
 	Category    string
 	Amount      int
 	PriceForOne int
-	Sum         int
+	Total       int
 }
 
 type OrderList struct {
 	list []Order
+	head Order
 }
 
 func main() {
@@ -36,24 +39,25 @@ func main() {
 
 	data := OrderList{}
 	record, e := reader.Read()
-	for {
+	data.AddHead(record)
+	for e == nil {
 		record, e = reader.Read()
-		if e != nil {
+		if e == io.EOF {
 			break
 		}
-		data.list = append(data.list, AddRow(record))
+		data.AddRow(record)
 	}
 
 	revenue := data.Revenue()
 	fmt.Printf("revenue: %v\n", revenue)
-
-	data.SortByAmount()
-	// quickSortStart(&data.list)
-	fmt.Printf("data: %v\n", data)
+	data.PrintOrderList()
+	data.SortBy(1)
+	data.PrintOrderList()
+	data.SortBy(2)
 	data.PrintOrderList()
 }
 
-func AddRow(s []string) Order { // Error
+func (data *OrderList) AddHead(s []string) {
 	var row Order
 	row.N, _ = strconv.Atoi(s[0])
 	row.DateOrder = s[1]
@@ -61,11 +65,23 @@ func AddRow(s []string) Order { // Error
 	row.Category = s[3]
 	row.Amount, _ = strconv.Atoi(s[4])
 	row.PriceForOne, _ = strconv.Atoi(s[5])
-	row.Sum, _ = strconv.Atoi(s[6])
-	if row.Valid() {
-		return row
+	row.Total, _ = strconv.Atoi(s[6])
+	data.head = row
+}
+
+func (data *OrderList) AddRow(s []string) { // Error
+	var row Order
+	row.N, _ = strconv.Atoi(s[0])
+	row.DateOrder = s[1]
+	row.NameProduct = s[2]
+	row.Category = s[3]
+	row.Amount, _ = strconv.Atoi(s[4])
+	row.PriceForOne, _ = strconv.Atoi(s[5])
+	row.Total, _ = strconv.Atoi(s[6])
+	if !row.Valid() {
+		panic("Data is Wrong")
 	}
-	panic("Data is Wrong")
+	data.list = append(data.list, row)
 }
 
 /*
@@ -74,7 +90,7 @@ Check: итоговая сумма должна быть равна произв
 Check: дата заказа не может быть позже текущей
 */
 func (x *Order) Valid() bool {
-	if x.Amount*x.PriceForOne != x.Sum {
+	if x.Amount*x.PriceForOne != x.Total {
 		return false
 	}
 	dateOrder, _ := time.Parse("01.02.2022", x.DateOrder)
@@ -84,19 +100,31 @@ func (x *Order) Valid() bool {
 	return true
 }
 
+// Считает общую выручку магазина
 func (data *OrderList) Revenue() int {
 	reven := 0
 	for _, v := range data.list {
-		reven += v.Sum
+		reven += v.Total
 	}
 	return reven
 }
 
-func partition(arr *[]Order, low, high int) int {
-	pivot := (*arr)[high].Amount
+func partition(arr *[]Order, low, high, param int) int {
+	var pivot int
+	if param == 1 {
+		pivot = (*arr)[high].Amount
+	} else {
+		pivot = (*arr)[high].Total
+	}
 	i := low
 	for j := low; j < high; j++ {
-		if (*arr)[j].Amount < pivot {
+		var current int
+		if param == 1 {
+			current = (*arr)[j].Amount
+		} else {
+			current = (*arr)[j].Total
+		}
+		if current < pivot {
 			(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
 			i++
 		}
@@ -104,25 +132,28 @@ func partition(arr *[]Order, low, high int) int {
 	(*arr)[i], (*arr)[high] = (*arr)[high], (*arr)[i]
 	return i
 }
-func quickSort(arr *[]Order, low, high int) {
+func quickSort(arr *[]Order, low, high, param int) {
 	if low < high {
-		var p int
-		p = partition(arr, low, high)
-		quickSort(arr, low, p-1)
-		quickSort(arr, p+1, high)
+		p := partition(arr, low, high, param)
+		quickSort(arr, low, p-1, param)
+		quickSort(arr, p+1, high, param)
 	}
 }
 
-// func quickSortStart(arr *[]Order) {
-// 	quickSort(arr, 0, len(*arr)-1)
-// }
-
-func (arr *OrderList) SortByAmount() {
-	quickSort(&arr.list, 0, len(arr.list)-1)
+/*
+Сортирует в зависимости от передаваемого параметра
+1 - Amount
+2 - Total
+*/
+func (arr *OrderList) SortBy(param int) {
+	quickSort(&arr.list, 0, len(arr.list)-1, param)
 }
 
 func (data *OrderList) PrintOrderList() {
+	fmt.Printf("%4v | %-30v | %-10v | %6v | %-8v \n",
+		"ID", data.head.NameProduct, "Количество", "Цена за товар", "Итого")
 	for _, v := range data.list {
-		fmt.Printf("%v | %-30v | Amount: %4v | Price: %6v | Total: %8v\n", v.N, v.NameProduct, v.Amount, v.PriceForOne, v.Sum)
+		fmt.Printf("%4v | %-30v | %-10v | %-13v | %-8v\n", v.N, v.NameProduct, v.Amount, v.PriceForOne, v.Total)
 	}
+	fmt.Println(strings.Repeat("=", 86))
 }
